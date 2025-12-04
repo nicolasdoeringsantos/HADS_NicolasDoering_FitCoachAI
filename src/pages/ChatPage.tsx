fed7a3bd8aff1583c16732fbe420ba7b53e7f79f
nicolasdoeringsantos <nicolasdoeringsantos@gmail.com>
2025-12-03 21:46:35 -0300

    placeholder

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -2,6 +2,6 @@ import  ChatComponent from './ChatComponent';
 
 export default function ChatPage() {
   return (
-    <ChatComponent chatType="treino" placeholder='test' title='Treino AI' initialMessage='Bem-vindo! Sou seu Treino AI, especialista em treinos personalizados. O que você gostaria de fazer hoje?' context="text"/>
+    <ChatComponent chatType="treino" placeholder='Peça seu plano de treino...' title='Treino AI' initialMessage='Bem-vindo! Sou seu Treino AI, especialista em treinos personalizados. O que você gostaria de fazer hoje?' context="text"/>
   );
 }
